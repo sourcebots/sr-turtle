b2fa0d58ec86cfc24fefebc09d5f63f243342b63
Alistair Lynn <arplynn@gmail.com>
2017-07-31 22:17:10 +0100

    Write a basic wall-following robot

--- test2.py
+++ test2.py
@@ -1,6 +1,32 @@
+from math import pi
+import time
+
 R = Robot()
 
 motor_board = R.motors[0]
 
 motor_board.m0.power = 50
-motor_board.m1.power = 50
+motor_board.m1.power = 0
+
+time.sleep(0.6)
+
+while True:
+    distance_right = R.send_ultrasound_ping(pi / 2) or 4.0
+    distance_ahead = R.send_ultrasound_ping(0) or 4.0
+
+    print("Forward distance: ", distance_ahead)
+
+    if distance_ahead < 1.0:
+        motor_board.m0.power = -50
+        motor_board.m1.power = 50
+        print("COLLISION AVOID")
+    else:
+        target_distance = 0.8
+        distance_error = distance_right - target_distance
+
+        print("Tracking error: ", distance_error, " - measured: ", distance_right)
+
+        motor_board.m0.power = 50
+        motor_board.m1.power = 50 - 35*distance_error
+
+    time.sleep(0.1)
