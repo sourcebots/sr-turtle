6794a1e8ac80d98a3f972ae20603460400e023c3
Alistair Lynn <arplynn@gmail.com>
2017-07-31 21:15:42 +0100

    Drop a vision import

--- sb/robot/__init__.py
+++ sb/robot/__init__.py
@@ -1,13 +1,2 @@
 from .simulator import Simulator
 from .sim_robot import SimRobot, AlreadyHoldingSomethingException
-from .vision import (
-    MARKER_ARENA,
-    MARKER_ROBOT,
-
-    MARKER_PEDESTAL,
-    MARKER_TOKEN,
-
-    MARKER_TOKEN_A,
-    MARKER_TOKEN_B,
-    MARKER_TOKEN_C,
-)
