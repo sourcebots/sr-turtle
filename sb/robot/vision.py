5e6db06e19a030ece8e6a008b51341dfc2956980
Andrew Barrett-Sprot <abarrettsprot@gmail.com>
2018-07-28 15:44:29 +0100

    Don't use NamedTuple as it's python 3.6
    We support python 3.5, not 3.6 so we can't use fancy NamedTuple
    initialisation

--- sb/robot/vision.py
+++ sb/robot/vision.py
@@ -1,28 +1,38 @@
-from typing import NamedTuple, Callable
-
+from collections import namedtuple
 
 # Points
 # TODO: World Coordinates
 
+# all floats
+PolarCoord = namedtuple(
+    'PolarCoord',
+    {'distance_meters', 'rot_y_rad', 'rot_y_deg'},
+)
+# distance_meters: float
+# rot_y_rad: float
+# rot_y_deg: float
 
-class PolarCoord(NamedTuple):
-    distance_meters: float
-    rot_y_rad: float
-    rot_y_deg: float
+CartCoord = namedtuple(
+    'CartCoord',
+   {'x', 'y', 'z'},
+)
+# x: float
+# y: float
+# z: float
 
+MarkerTuple = namedtuple(
+    'Marker',
+    {'id', 'size', 'polar', 'is_wall_marker', 'is_token_marker'}
+)
+# id: int
+# size: float
+# polar: PolarCoord
+# is_wall_marker: Callable[[], bool]
+# is_token_marker: Callable[[], bool]
 
-class CartCoord(NamedTuple):
-    x: float
-    y: float
-    z: float
-
-
-class Marker(NamedTuple):
-    id: int
-    size: float
-    polar: PolarCoord
-    is_wall_marker: Callable[[], bool]
-    is_token_marker: Callable[[], bool]
+class Marker(MarkerTuple):
+    __slots__ = ()
 
     def __str__(self):
-        return "Marker {}, pos: ({},{})".format(self.id, self.polar.distance_meters, self.polar.rot_y_rad)
+        return "Marker {}, pos: ({},{})".format(self.id, self.polar.distance_meters,
+                                                self.polar.rot_y_rad)
