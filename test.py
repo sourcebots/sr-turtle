9189dcb45fbd7f7948021a9b4432c7416902f9a9
Peter Law <PeterJCLaw@gmail.com>
2018-08-04 19:05:35 +0100

    Adapt test.py to the SourceBots API

--- test.py
+++ test.py
@@ -1,19 +1,19 @@
 from math import pi
 import time
 
-R = Robot()
+r = Robot()
 
-motor_board = R.motor_board
+motor_board = r.motor_board
 
 while True:
-    distance_right = R.servo_board.read_ultrasound(8, 9)
-    distance_ahead = R.servo_board.read_ultrasound(6, 7)
+    distance_right = r.servo_board.read_ultrasound(8, 9)
+    distance_ahead = r.servo_board.read_ultrasound(6, 7)
 
     print("Forward distance: ", distance_ahead)
 
     if distance_ahead < 1.0:
-        motor_board.m0.voltage = -0.5
-        motor_board.m1.voltage = 0.5
+        motor_board.m0 = -0.5
+        motor_board.m1 = 0.5
         print("COLLISION AVOID")
     else:
         target_distance = 0.8
@@ -21,7 +21,7 @@ while True:
 
         print("Tracking error: ", distance_error, " - measured: ", distance_right)
 
-        motor_board.m0.voltage = 0.5
-        motor_board.m1.voltage = 0.5 - .35 * distance_error
+        motor_board.m0 = 0.5
+        motor_board.m1 = 0.5 - .35 * distance_error
 
     time.sleep(0.1)
